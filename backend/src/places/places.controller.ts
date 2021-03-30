ec086da032524cb0204a4ec3657e0d9c59d7e4f9
Alex <csiszaralex@gmail.com>
2021-03-30 21:35:28 +0200

    :alembic: Add Swagger to every endpoint

--- backend/src/places/places.controller.ts
+++ backend/src/places/places.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { PlacesService } from './places.service';
 import { CreatePlaceDto } from './dto/create-place.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('check-in')
 export class PlacesController {
   constructor(private readonly placesService: PlacesService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Új hely létrehozása' })
+  @ApiResponse({ status: 201, description: 'Hely' })
+  @ApiResponse({ status: 400, description: 'Hibás adatok küldve' })
+  @ApiResponse({ status: 500, description: 'Szerverhiba' })
   createPlace(@Body(ValidationPipe) createPlaceDto: CreatePlaceDto) {
     return this.placesService.createPlace(createPlaceDto);
   }
--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
+  @ApiProperty({ example: 'Kerek Elek' })
+  name: string;
+
+  @IsNotEmpty()
+  @ApiProperty({ example: 'example@gmail.com' })
   email: string;
 
   @IsNotEmpty()
+  @ApiProperty({ example: 'Alma1234' })
   password: string;
-
-  @IsNotEmpty()
-  name: string;
 }
