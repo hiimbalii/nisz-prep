034bc3eeb0dd2dbc5fbb2d25d9af5a15ee93ee87
Alex <csiszaralex@gmail.com>
2021-04-07 12:09:05 +0200

    :recycle: Change permission endpoints with authentication

--- backend/src/permissions/permissions.controller.ts
+++ backend/src/permissions/permissions.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe, UseGuards } from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { PermissionsGuard } from 'src/users/guards/permissions.guard';
+import { Permissions } from 'src/users/decorators/permissions.decorator';
 
 @ApiTags('Permission')
 @Controller('permissions')
@@ -9,9 +12,14 @@ export class PermissionsController {
   constructor(private readonly permissionsService: PermissionsService) {}
 
   @Post()
+  @Permissions('PERMISSION')
+  @UseGuards(PermissionsGuard)
+  @UseGuards(AuthGuard())
   @ApiOperation({ summary: 'Jogosultság létrehozása' })
-  @ApiResponse({ status: 200, description: 'Sikeres művelet' })
+  @ApiResponse({ status: 201, description: 'Sikeres művelet' })
   @ApiResponse({ status: 400, description: 'Hibásan megadott adatok' })
+  @ApiResponse({ status: 401, description: 'Hibás token' })
+  @ApiResponse({ status: 403, description: 'Nincs jogosultság' })
   @ApiResponse({ status: 409, description: 'Már létezik ilyen kódú jogosutság' })
   @ApiResponse({ status: 500, description: 'Szerverhiba' })
   createPermission(@Body(ValidationPipe) createPermissionDto: CreatePermissionDto) {
--- backend/src/permissions/permissions.module.ts
+++ backend/src/permissions/permissions.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
 import { PermissionsController } from './permissions.controller';
 import { PermissionsRepository } from './permissions.repository';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   controllers: [PermissionsController],
   providers: [PermissionsService, PermissionsRepository],
-  exports: [PermissionsModule]
+  exports: [PermissionsModule],
+  imports: [UsersModule],
 })
 export class PermissionsModule {}
