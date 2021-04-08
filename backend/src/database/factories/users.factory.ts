49ab849ddeb1663da8e7a872913900dfff4259e6
borbelybalint <bbalint0404@gmail.com>
2021-04-08 18:18:00 +0200

    :seedling: Removes unnecessary constant

--- backend/src/database/factories/users.factory.ts
+++ backend/src/database/factories/users.factory.ts
@@ -5,14 +5,13 @@ import { genSaltSync, hashSync } from 'bcrypt';
 define(User, faker => {
   const salt = genSaltSync();
   const isInfected = faker.random.boolean();
-  const morning = faker.random.boolean();
 
   const user = new User();
   user.name = faker.name.firstName(0) + ' ' + faker.name.lastName(0);
   user.password = hashSync('Alma', salt);
   user.salt = salt;
   user.email = faker.internet.email();
-  user.morning = isInfected ? morning : null;
+  user.morning = isInfected ? faker.random.boolean() : null;
   user.infectedDate = isInfected ? faker.date.between('2019.03.25', '2021.05.03') : new Date(0);
 
   return user;
